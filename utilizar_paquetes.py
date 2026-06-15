e2c288199f588c73cf69cd32e1c7f7aa3158fae6
Valerie-padilla <valerie_3042250055@utd.edu.mx>
2026-06-14 21:38:03 -0600

    Add files via upload

---
+++ utilizar_paquetes.py
@@ -0,0 +1,29 @@
+from paquete1 import modulos, modulo_paquete
+
+modulos.borrarPantalla()
+
+nom="juan"
+
+ape="polancas" 
+
+nombre, apellido=modulos.funcion4(nom, ape)
+
+print("\330c")
+
+print(f"nombre {nombre} \ apellidos {apellido}")
+
+from paquete1 import modulos
+
+modulos.borrarPantalla()
+
+def solicitar_edad():
+    edad=int(input("Edad: "))
+    return edad
+
+edad=int(input("Edad: "))
+
+nom="Juan"
+ape="Polainas"
+nombre, apellidos=modulos.funcion4(nom, ape)
+edad=modulo_paquete
+print(f"Nombre es: {nombre} \n Apellidos: {apellidos}\n Edad: {edad}")
